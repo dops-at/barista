af4605433fbd2817182ad8e703edfe7c5e38ee04
Lukas Holzer <lukas.holzer@dynatrace.com>
2020-09-17 08:40:04 +0000

    chore: Drop this test commit later

---
+++ test.js
@@ -0,0 +1,13 @@
+console.log(`cwd: ${process.cwd()}`);
+
+// const path = require('path');
+// const runfiles = require(process.env['BAZEL_NODE_RUNFILES_HELPER']);
+// const args = process.argv.slice(2);
+
+// console.log(`
+
+// cwd: ${process.cwd()}
+// Workspace Relative: ${runfiles.resolveWorkspaceRelative(args[1])}
+// Relative: ${path.resolve(args[1])}
+
+// `)
